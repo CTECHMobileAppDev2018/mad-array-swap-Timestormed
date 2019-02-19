3dcdcc41b8c5edec78374e66684f8fdaef333a4a
Issac <issac.a.olson@gmail.com>
2019-02-19 08:29:17 -0600

    2/19 Push

--- ArraySwapper.java
+++ ArraySwapper.java
@@ -3,25 +3,33 @@ import java.util.Arrays;
 class ArraySwapper {
   
   // This method assumes you only have 2 items in your array
-  public void arraySwap(int []inputArray) {
+  public void arraySwap(int []arrVar) {
     
-    // Swap the two items in "inputArray" and return the output
+    
+    // Swap the two items in "arrVar" and return the output
     // put your code here
-    int temp;
-        
+    
+    int temp; // you can use this as a temporary variable
+    temp = arrVar[0];
+    arrVar[0] = arrVar[1];
+    arrVar[1] = temp;
   }
   
   // This method swaps any 2 items in any array
-  public void arraySwapAny2(int []inputArray, int loc1, int loc2) {
+  public void arraySwapAny2(int []arrVar, int loc1, int loc2) {
     
-    // Swap the two items in "inputArray" and return the output
+    // Swap the two items in "arrVar" and return the output
     // Remember to make sure that loc1 and loc2 are valid positions in the array!
-    int temp;
-        
+    
+      int temp; // you can use this as a temporary variable
+    temp = arrVar[loc1];
+    arrVar[loc1] = arrVar[loc2];
+    arrVar[loc2] = temp;
   }
+    
+        
+  
 
-  
-  
   public static void main(String[] args) {
 
     ArraySwapper testSwapper = new ArraySwapper();
@@ -31,7 +39,7 @@ class ArraySwapper {
     int[] exampleInput = {1, 2};
     int[] exampleOutput = {2, 1};
     System.out.println("exampleInput before: " + Arrays.toString(exampleInput));    
-    System.out.println("exampleOutput: " + Arrays.toString(exampleOutput));    
+    System.out.println("exampleOutput: " + Arrays.toString(exampleInput));    
     
     // This calls the first swap method
     testSwapper.arraySwap(exampleInput);
@@ -40,9 +48,9 @@ class ArraySwapper {
     if (java.util.Arrays.equals(exampleInput, exampleOutput)) {
       System.out.println("arraySwap succeeded");
     } else {
-      System.out.println("arraySwap failed!");
+      System.out.println("arraySwap FAILED!");
     }
-    
+   
     // ***** Test the second swap method *******
     System.out.println("\n*** Trying second method ***");
     exampleInput = new int[]{1, 22, 333, 4444};
@@ -57,7 +65,7 @@ class ArraySwapper {
     if (java.util.Arrays.equals(exampleInput, exampleOutput)) {
       System.out.println("arraySwapAny2 succeeded");
     } else {
-      System.out.println("arraySwapAny2 failed!");
+      System.out.println("arraySwapAny2 FAILED!");
     }
   }
 }
